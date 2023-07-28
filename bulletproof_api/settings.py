0e350bb3f2b50fcaacbb2b3b3f62c653273a8f80
rahimbtc <rahim.bait.2021@gmail.com>
2023-07-28 19:15:41 +0100

    change static

--- bulletproof_api/settings.py
+++ bulletproof_api/settings.py
@@ -209,10 +209,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
-]
 
 # Set the base directory for media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
